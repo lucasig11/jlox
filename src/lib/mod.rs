8730f2a0f4ebb73b7915fc3c337c0266699f8fb7
lucas <lucasfonsecard@icloud.com>
2021-09-11 03:51:45 -0300

    feat: evaluate expressions in REPL

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,6 +1,6 @@
-use error::{InterpreterError, LoxResult};
+use error::{InterpreterError, LoxError, LoxResult};
 use interpreter::Resolver;
-use std::{io::Write, path::PathBuf};
+use std::{io::Write, path::PathBuf, rc::Rc};
 
 mod error;
 mod interpreter;
@@ -14,54 +14,81 @@ use lexer::Lexer;
 use interpreter::Interpreter;
 use parser::Parser;
 
+use self::{interpreter::Environment, parser::Stmt};
+
 pub struct Lox;
 
 impl Lox {
     pub(crate) fn do_file(path: PathBuf) -> LoxResult<()> {
         let src = std::fs::read_to_string(&path)?;
-        Self::run(
-            &src,
-            path.to_str().ok_or_else(|| {
-                std::io::Error::new(
-                    std::io::ErrorKind::InvalidData,
-                    "filename contains illegal UTF-8 characters",
-                )
-            })?,
-        );
+
+        if src.trim().is_empty() {
+            return Ok(());
+        }
+
+        std::env::set_var("LOX_SRC_FILE", &*path.to_string_lossy());
+
+        let run = |src| {
+            let statements = Self::parse(src)?;
+            Self::execute(statements)
+        };
+
+        if let Err(errors) = run(src.to_string()) {
+            for e in errors {
+                let e = InterpreterError::from(e, &src);
+                eprintln!("{}\n", e);
+            }
+        }
         Ok(())
     }
 
     pub(crate) fn do_repl() -> LoxResult<()> {
+        let mut buf = String::with_capacity(200_000);
+        std::env::set_var("LOX_SRC_FILE", "REPL");
+        let env = Rc::new(Environment::new());
         loop {
             print!("> ");
-            let mut buf = String::new();
             std::io::stdout().flush()?;
             std::io::stdin().read_line(&mut buf)?;
-            Self::run(&buf, "stdin");
+
+            let run = |src| {
+                let statements = Self::parse(src)?;
+                if statements.len().eq(&1) {
+                    if let Some(Stmt::Expression(expr)) = statements.first() {
+                        println!(
+                            "{}",
+                            expr.evaluate(Rc::clone(&env), &Default::default())
+                                .map_err(|e| vec![e])?
+                                .to_string()
+                        );
+                        return Ok(());
+                    }
+                }
+                Self::execute(statements)
+            };
+
+            if let Err(errors) = run(buf.to_string()) {
+                for e in errors {
+                    let e = InterpreterError::from(e, &buf);
+                    eprintln!("{}\n", e);
+                }
+            }
+            buf.clear();
         }
     }
 
-    fn run<T: AsRef<str>>(src: &str, src_filename: T) {
-        if src.trim().is_empty() {
-            return;
+    fn parse(src: String) -> Result<Vec<Stmt>, Vec<LoxError>> {
+        let tokens = Lexer::new(&src).scan_tokens().map_err(|e| vec![e])?;
+        Parser::new(&tokens).parse()
+    }
+
+    fn execute(statements: Vec<Stmt>) -> Result<(), Vec<LoxError>> {
+        if statements.is_empty() {
+            return Ok(());
         }
-
-        std::env::set_var("LOX_SRC_FILE", src_filename.as_ref());
-
-        let run = |src| {
-            let tokens = Lexer::new(src).scan_tokens().map_err(|e| vec![e])?;
-            let statements = Parser::new(&tokens).parse()?;
-            let interpreter = Interpreter::new(&statements);
-            let resolver = Resolver::new(&interpreter);
-            resolver.resolve(&statements).map_err(|e| vec![e])?;
-            interpreter.interpret()
-        };
-
-        if let Err(errors) = run(src) {
-            for e in errors {
-                let e = InterpreterError::from(e, src);
-                eprintln!("{}\n", e);
-            }
-        }
+        let interpreter = Interpreter::new(&statements);
+        let resolver = Resolver::new(&interpreter);
+        resolver.resolve(&statements).map_err(|e| vec![e])?;
+        interpreter.interpret()
     }
 }
